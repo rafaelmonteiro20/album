f87a10d88cb1a43e37668ffeecf001385a15b33a
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-04-26 09:25:01 -0300

    Tipando retorno da API

--- ngapp/src/app/photos/photo/photo.service.ts
+++ ngapp/src/app/photos/photo/photo.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Photo } from './photo';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
     constructor(private http: HttpClient) {}
 
-    listAll(): Observable<Object[]> {
-        return this.http.get<Object[]>('http://localhost:8080/photos');
+    listAll(): Observable<Photo[]> {
+        return this.http.get<Photo[]>('http://localhost:8080/photos');
     }
+    
 }
