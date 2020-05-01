1aeb398ffecd8601191b0118e53c00f434b64c2e
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-01 13:57:53 -0300

    Criando componente photos

--- ngapp/src/app/photos/photos.module.ts
+++ ngapp/src/app/photos/photos.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
     declarations: [
         PhotoComponent,
-        PhotoListComponent
+        PhotoListComponent,
+        PhotosComponent
     ],
     imports: [
         CommonModule,
