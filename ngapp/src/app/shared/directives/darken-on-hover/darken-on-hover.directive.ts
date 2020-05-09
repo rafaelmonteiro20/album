780c5ff35209f91f1536e91701def5daa1f72363
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-08 21:47:55 -0300

    Finalizando implementação da diretiva DarkenOnHover

--- ngapp/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ ngapp/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
     selector: '[appDarkenOnHover]'
 })
 export class DarkenOnHoverDirective {
 
+    constructor(private el: ElementRef, private render: Renderer) {}
+
     @HostListener('mouseover')
     darkenOn() {
-        console.log('darkenOn');
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(92%)');
     }
 
     @HostListener('mouseleave')
     darkenOff() {
-        console.log('darkenOff')
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
     }
 
 }
