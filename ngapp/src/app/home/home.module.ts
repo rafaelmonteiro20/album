bb17189ed3f648d266e306aa86501b1254eeb2fc
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-09 14:22:41 -0300

    Validando formulário de login

--- ngapp/src/app/home/home.module.ts
+++ ngapp/src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
     declarations: [
         SignInComponent
+    ],
+    imports: [
+        ReactiveFormsModule
     ]
 })
 export class HomeModule { }
