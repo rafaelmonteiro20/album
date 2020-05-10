722dfe679f3ef5a3b410825fcd975130c93ac671
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-09 23:18:56 -0300

    Ativando lombok no projeto

--- src/main/java/com/album/model/Photo.java
+++ src/main/java/com/album/model/Photo.java
@@ -1,5 +1,8 @@
 package com.album.model;
 
+import lombok.Data;
+
+@Data
 public class Photo {
 
 	private String description;
@@ -7,45 +10,9 @@ public class Photo {
 	private int likes;
 	private int comments;
 
-	public Photo() {
-
-	}
-	
 	public Photo(String description, String url) {
 		this.description = description;
 		this.url = url;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public int getLikes() {
-		return likes;
-	}
-
-	public void setLikes(int likes) {
-		this.likes = likes;
-	}
-
-	public int getComments() {
-		return comments;
-	}
-
-	public void setComments(int comments) {
-		this.comments = comments;
 	}
 
 }
