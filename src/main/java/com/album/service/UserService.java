79b6d123a06de4b666a1957201261eb91cf810ca
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-10 19:15:00 -0300

    Criptografando senha ao salvar usuário

--- src/main/java/com/album/service/UserService.java
+++ src/main/java/com/album/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.album.model.User;
@@ -16,13 +17,16 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 	
 	@Transactional
 	public User save(User user) {
-		user.setPassword(user.getUsername());
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 	
