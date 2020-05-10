ce13e89641a13caa68e7038b2371db38eb88c156
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-10 19:08:04 -0300

    Configurando sequence na entidade user

--- src/main/java/com/album/model/User.java
+++ src/main/java/com/album/model/User.java
@@ -4,7 +4,10 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -14,10 +17,12 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Table(name = "\"user\"")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SequenceGenerator(name = "seq_user", sequenceName = "seq_user", allocationSize = 1)
 public class User {
 
 	@Id
 	@Column(name = "id_user")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
 	@EqualsAndHashCode.Include
 	private Long id;
 	
