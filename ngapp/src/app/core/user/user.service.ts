bbea47a8bb24533d0b0cefd752a4ee2b14f5c266
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-15 22:17:53 -0300

    Decodificando token jwt

--- ngapp/src/app/core/user/user.service.ts
+++ ngapp/src/app/core/user/user.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode'
+
 import { TokenService } from '../token/token.service';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-    constructor(private tokenService: TokenService) { }
+    constructor(private tokenService: TokenService) {
+        this.tokenService.hasToken() && this.decodeAndNotify();
+    }
 
     setToken(token: string) {
         this.tokenService.setToken(token);
+        this.decodeAndNotify();
     }
 
     getUser() {
 
     }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token);
+        console.log(user);
+    }
+
 }
