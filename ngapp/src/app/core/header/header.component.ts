d00d3c2b4aa10f656fa427ae09f99f3e1e6d83cd
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-16 21:03:41 -0300

    Exibindo usuário logado

--- ngapp/src/app/core/header/header.component.ts
+++ ngapp/src/app/core/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+
+import { User } from '../user/user';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+    user: User;
+
+    constructor(userService: UserService) {
+        const user$ = userService.getUser();
+        user$.subscribe(user => this.user = user);
+    }
 
 }
