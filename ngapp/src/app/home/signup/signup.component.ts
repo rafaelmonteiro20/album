47de915cbcb2f5aaeacb384dcbdca1f085b8a19a
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-18 21:05:48 -0300

    Validando formuário de cadastro de usuário

--- ngapp/src/app/home/signup/signup.component.ts
+++ ngapp/src/app/home/signup/signup.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+
+    signupForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) { }
+    
+    ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            email: ['', 
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(50)
+                ]
+            ],
+            userName: ['',
+                [
+                    Validators.required,
+                    Validators.pattern(/^[a-z0-9_\-]+$/),
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.maxLength(14)
+                ]
+            ]
+        });
+    }
 
 }
