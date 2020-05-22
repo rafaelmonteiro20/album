997344a32bdafc81b187abeaf2cc0afbd5ba8d42
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-21 21:45:45 -0300

    Impedindo usuário acessar página de login quando logado

--- ngapp/src/app/core/auth/auth.guard.ts
+++ ngapp/src/app/core/auth/auth.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 import { UserService } from '../user/user.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
-    constructor(private userService: UserService) { }
+    constructor(
+        private userService: UserService, 
+        private router: Router) { }
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (this.userService.isLogged()) {
+            this.router.navigate(['user', 'photos']);
+            return false;
+        }
         return true;
     }
 
