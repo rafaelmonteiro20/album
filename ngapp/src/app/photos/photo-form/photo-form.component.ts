93c267c73a73d949fe2abd0e831ed30a5dfa316b
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-25 21:43:48 -0300

    Validando formulário de upload de fotos

--- ngapp/src/app/photos/photo-form/photo-form.component.ts
+++ ngapp/src/app/photos/photo-form/photo-form.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './photo-form.component.html'
 })
-export class PhotoFormComponent {
+export class PhotoFormComponent implements OnInit {
+
+    photoForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+    
+    ngOnInit(): void {
+        this.photoForm = this.formBuilder.group({
+            file: ['', Validators.required],
+            description: ['', 
+                [
+                    Validators.required,
+                    Validators.maxLength(400)
+                ]
+            ],
+            allowComments: [true]
+        });
+    }
 
 }
