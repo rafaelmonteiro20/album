ac2f230119787017e8d4334aca533ef9f88a7eac
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-05-31 15:06:10 -0300

    Ajustando busca de foto no front-end

--- ngapp/src/app/photos/photo/photo.component.ts
+++ ngapp/src/app/photos/photo/photo.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:8080/images/';
+
 @Component({
     selector: 'app-photo',
     templateUrl: './photo.component.html'
 })
 export class PhotoComponent {
 
+    private _url = '';
+
     @Input() description = '';
-    @Input() url = '';
+    
+    @Input() set url(url: string) {
+        if (url.startsWith('http')) {
+            this._url = url;
+        } else {
+            this._url = CLOUD + url;
+        }
+    }
+
+    get url() {
+        return this._url;
+    }
     
 }
